cd97f0a900657f6361be6df762fb932eb78f39a5
Aditya Kumar <aditya00447@gmail.com>
2026-04-22 01:06:49 +0530

    Completed this project

--- server/src/app.js
+++ server/src/app.js
@@ -8,7 +8,7 @@ const app = express();
 
 
 // Middlewares 
-app.use(cors({credentials:true}));
+app.use(cors({ origin: "http://localhost:5173",credentials:true}));
 app.use(express.json());// to send json responses
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -10,7 +10,7 @@ router.post('/login', login);
 router.post('/logout', logout);
 router.post('/send-verify-otp', userAuth, sendVerifyOtp);
 router.post('/verify-account', userAuth, verifyEmail);
-router.post('/is-auth', userAuth, isAuthenticated);
+router.get('/is-auth', userAuth, isAuthenticated);
 router.post('/send-reset-otp', sendPassResetOtp);
 router.post('/resetPassword', resetPass);
 
